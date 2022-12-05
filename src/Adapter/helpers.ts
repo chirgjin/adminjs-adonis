b4b690488914e0ac71b98305c8426861198891aa
Chirag <chirgjin@gmail.com>
2022-12-06 00:06:14 +0530

    feat: helper to apply default values to options

---
+++ src/Adapter/helpers.ts
@@ -0,0 +1,29 @@
+import type { AdminColumnOptions } from '@ioc:Adonis/Addons/AdminJS'
+import type { LucidModel } from '@ioc:Adonis/Lucid/Orm'
+
+/**
+ * Helper function to get admin column options for given model & column.
+ *
+ * Adds default values to the options not provided by the user
+ */
+export function getAdminColumnOptions(model: LucidModel, columnKey: string) {
+    const providedOptions =
+        model.$adminColumnOptions && model.$adminColumnOptions[columnKey]
+    const columnOptions = model.$getColumn(columnKey)
+
+    if (!columnOptions) {
+        throw new Error(`${model.name} doesn't have ${columnKey} column!`)
+    }
+
+    return {
+        name: providedOptions?.name ?? columnOptions.columnName,
+        position: providedOptions?.position ?? 1,
+        type: providedOptions?.type || columnOptions.meta?.type,
+        visible: providedOptions?.visible ?? true,
+        editable: providedOptions?.editable ?? !columnOptions.isPrimary,
+        unique: providedOptions?.unique ?? columnOptions.isPrimary,
+        enum: providedOptions?.enum,
+        sortable: providedOptions?.sortable || false,
+        optional: providedOptions?.optional || false,
+    } as AdminColumnOptions
+}
