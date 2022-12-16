f61b421522026cff3c320c5389429b525922b849
Chirag <chirgjin@gmail.com>
2022-12-16 15:40:18 +0530

    refactor: turn router into class

--- src/Plugin/router.ts
+++ src/Plugin/router.ts
@@ -1,79 +1,83 @@
+import { inject } from '@adonisjs/core/build/standalone'
 import AdminJS, { Router as AdminRouter, RouterType } from 'adminjs'
 
 import type { RouterConfig } from '@ioc:Adonis/Addons/AdminJS'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import type { RouterContract } from '@ioc:Adonis/Core/Route'
 
-/**
- * Helper function to create handler for a given adminjs route
- */
-export function createRouteHandler(
-    admin: AdminJS,
-    route: RouterType['routes'][number]
-) {
-    return async (ctx: HttpContextContract) => {
-        const controller = new route.Controller(
-            {
-                admin,
-            },
-            (ctx as any).auth?.user // if auth plugin is installed then get user from it
-        )
+@inject([null, null, 'Adonis/Core/Route'])
+export class Router {
+    constructor(
+        protected admin: AdminJS,
+        protected config: RouterConfig,
+        private route: RouterContract
+    ) {}
 
-        const html = await controller[route.action](
-            {
-                ...ctx.request,
-                params: ctx.params,
-                query: ctx.request.qs(),
-                payload: {
-                    ...ctx.request.body(),
-                    ...ctx.request.allFiles(),
+    /**
+     * Helper function to create handler for a given adminjs route
+     */
+    public createRouteHandler(route: RouterType['routes'][number]) {
+        return async (ctx: HttpContextContract) => {
+            const controller = new route.Controller(
+                {
+                    admin: this.admin,
                 },
-                method: ctx.request.method().toLowerCase(),
-            },
-            ctx.response
-        )
+                (ctx as any).auth?.user // if auth plugin is installed then get user from it
+            )
 
-        if (route.contentType) {
-            ctx.response.header('content-type', route.contentType)
+            const html = await controller[route.action](
+                {
+                    ...ctx.request,
+                    params: ctx.params,
+                    query: ctx.request.qs(),
+                    payload: {
+                        ...ctx.request.body(),
+                        ...ctx.request.allFiles(),
+                    },
+                    method: ctx.request.method().toLowerCase(),
+                },
+                ctx.response
+            )
+
+            if (route.contentType) {
+                ctx.response.header('content-type', route.contentType)
+            }
+
+            return ctx.response.send(html)
+        }
+    }
+
+    /**
+     * Helper function to create handler to serve a given asset
+     */
+    public createAssetHandler(asset: RouterType['assets'][number]) {
+        return ({ response }: HttpContextContract) => {
+            return response.download(asset.src)
+        }
+    }
+
+    /**
+     * Helper function to build routes for an adminjs instance
+     */
+    public buildRoutes() {
+        if (!this.config.enabled) {
+            return
         }
 
-        return ctx.response.send(html)
+        this.route
+            .group(() => {
+                AdminRouter.assets.forEach((asset) => {
+                    this.route.get(asset.path, this.createAssetHandler(asset))
+                })
+
+                AdminRouter.routes.forEach((route) => {
+                    this.route[route.method.toLowerCase()](
+                        route.path.replace(/\{/g, ':').replace(/\}/g, ''),
+                        this.createRouteHandler(route)
+                    )
+                })
+            })
+            .prefix(this.config.routePrefix || '/')
+            .middleware(this.config.middlewares || [])
     }
-}
-
-/**
- * Helper function to create handler to serve a given asset
- */
-export function createAssetHandler(asset: RouterType['assets'][number]) {
-    return ({ response }: HttpContextContract) => {
-        return response.download(asset.src)
-    }
-}
-
-/**
- * Helper function to build routes for an adminjs instance
- */
-export function buildRoutes(
-    admin: AdminJS,
-    config: RouterConfig,
-    Route: RouterContract
-) {
-    if (!config.enabled) {
-        return
-    }
-
-    Route.group(() => {
-        AdminRouter.assets.forEach((asset) => {
-            Route.get(asset.path, createAssetHandler(asset))
-        })
-
-        AdminRouter.routes.forEach((route) => {
-            Route[route.method.toLowerCase()](
-                route.path.replace(/\{/g, ':').replace(/\}/g, ''),
-                createRouteHandler(admin, route)
-            )
-        })
-    })
-        .prefix(config.routePrefix || '/')
-        .middleware(config.middlewares || [])
 }
