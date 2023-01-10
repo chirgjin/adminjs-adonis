661f76194c706a4ef43ed9027b8905ad67216c6c
Chirag <chirgjin@gmail.com>
2023-01-10 22:36:02 +0530

    fix: path to file input

--- src/Adapter/Components/index.ts
+++ src/Adapter/Components/index.ts
@@ -3,7 +3,7 @@ import { ComponentLoader } from 'adminjs'
 export const componentLoader = new ComponentLoader()
 
 export const components = {
-    FileInput: componentLoader.add('FileInput', './fileInput'),
+    FileInput: componentLoader.add('FileInput', './FileInput'),
     ListUrl: componentLoader.add('ListUrl', './ListUrl'),
     ShowUrl: componentLoader.add('ShowUrl', './ShowUrl'),
 }
