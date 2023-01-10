692232ed75281756a45983599d38975192a3484e
Chirag <chirgjin@gmail.com>
2023-01-10 19:12:09 +0530

    feat: hook decorators

--- src/Adapter/decorators.ts
+++ src/Adapter/decorators.ts
@@ -1,5 +1,12 @@
-import type { AdminColumnOptions } from '@ioc:Adonis/Addons/AdminJS'
-import type { LucidModel, LucidRow } from '@ioc:Adonis/Lucid/Orm'
+import type {
+    AdminColumnOptions,
+    HookDecorator,
+} from '@ioc:Adonis/Addons/AdminJS'
+import type {
+    LucidModel,
+    LucidRow,
+    ModelQueryBuilderContract,
+} from '@ioc:Adonis/Lucid/Orm'
 
 /**
  * Define type, optional etc properties for AdminJS
@@ -13,3 +20,79 @@ export function adminColumn(options: Partial<AdminColumnOptions>) {
         model.$adminColumnOptions![property] = options
     }
 }
+
+export const beforeCreate: HookDecorator<LucidRow> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add(
+            'before',
+            'adminCreate',
+            target[property].bind(target)
+        )
+    }
+
+export const beforeUpdate: HookDecorator<LucidRow> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add(
+            'before',
+            'adminUpdate',
+            target[property].bind(target)
+        )
+    }
+
+export const beforeDelete: HookDecorator<LucidRow> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add(
+            'before',
+            'adminDelete',
+            target[property].bind(target)
+        )
+    }
+
+export const beforeFind: HookDecorator<
+    ModelQueryBuilderContract<LucidModel>
+> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add('before', 'adminFind', target[property].bind(target))
+    }
+
+export const beforeFetch: HookDecorator<
+    ModelQueryBuilderContract<LucidModel>
+> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add('before', 'adminFetch', target[property].bind(target))
+    }
+
+export const afterCreate: HookDecorator<LucidRow> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add('after', 'adminCreate', target[property].bind(target))
+    }
+
+export const afterUpdate: HookDecorator<LucidRow> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add('after', 'adminUpdate', target[property].bind(target))
+    }
+
+export const afterDelete: HookDecorator<LucidRow> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add('after', 'adminDelete', target[property].bind(target))
+    }
+
+export const afterFind: HookDecorator<LucidRow> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add('after', 'adminFind', target[property].bind(target))
+    }
+
+export const afterFetch: HookDecorator<LucidRow[]> = () =>
+    function (target, property) {
+        target.boot()
+        target.$hooks.add('after', 'adminFetch', target[property].bind(target))
+    }
